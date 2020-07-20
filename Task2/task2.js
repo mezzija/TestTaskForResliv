fc770ec4ce2c2f46d1ba85498933a3f8ca5a8076
mezzija <mezzija@mail.ru>
2020-07-20 21:21:13 +0300

    task2

--- Task2/task2.js
+++ Task2/task2.js
@@ -2,41 +2,37 @@ const url = 'http://любой_домен/filter?size=M&color=1&color=2&manufact
 
 
 const parameters = url.slice(url.indexOf('?') + 1).split('&');
-const root = document.getElementById('root');
 
-const form1 = document.createElement('form');
-const form2 = document.createElement('form');
-const form3 = document.createElement('form')
 
-form1.innerHTML = `
-           <input type="radio" name="size" id="S">
-            <label for="s">S</label>
-            <input type="radio" name="size" id="M">
-            <label for="m">M</label>
-            <input type="radio"  name="size" id="L">
-            <label for="l">L</label>
-`;
-form2.innerHTML = `
-           <input type="checkbox" name="color" id="1" >
-            <label for="checkbox1">1</label>
-            <input type="checkbox" name="color" id="2">
-            <label for="checkbox2">2</label>
-            <input type="checkbox" name="color" id="3">
-            <label for="checkbox3">3</label>
-            <input type="checkbox" name="color" id="4">
-            <label for="checkbox4">4</label>
-            <input type="checkbox" name="color" id="5">
-            <label for="checkbox5">5</label>
-`;
-form3.innerHTML = `
-          <select name="select" id="select" multiple >
-            <option value="aaa">aaa</option>
-            <option value="b&c">b&c</option>
-            <option value="ddd">ddd</option>
-            <option value="eee">eee</option>
-        </select>
-`;
+const form1 = document.getElementById('radio');
+const form2 = document.getElementById('checkbox');
+const form3 = document.getElementById('selected')
 
+const form=document.getElementById('form');
+
+form.addEventListener('change',event=>{
+    const changeUrl=[]
+    form1.childNodes.forEach(item=>{
+        if(item.checked){
+            changeUrl.push(`size=${item.id}`)
+        }
+    })
+    form2.childNodes.forEach(item=>{
+        if(item.checked){
+            changeUrl.push(`color=${item.id}`)
+        }
+    })
+    form3.firstElementChild.childNodes.forEach(item=>{
+        if(item.selected){
+            changeUrl.push(`manufacturer=${item.value}`)
+        }
+    })
+    if(event.target.id!=='sale'){
+        console.log(`http://любой_домен/filter?`+changeUrl.join('&'));
+    }
+
+
+})
 
 parameters.forEach(item => {
     if (item.slice(0, item.indexOf('=')) === 'size') {
@@ -61,7 +57,3 @@ parameters.forEach(item => {
         })
     }
 })
-
-root.appendChild(form1)
-root.appendChild(form2)
-root.appendChild(form3)
